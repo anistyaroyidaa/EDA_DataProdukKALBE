06703b2c61e6dd46c42df10e862dd050adf82bda
anistyaroyidaa <anistya.88@students.amikom.ac.id>
2023-09-06 07:07:48 +0700

    edit file sql

--- AnistyaRosyida_KALBE_DS_Query.sql
+++ AnistyaRosyida_KALBE_DS_Query.sql
@@ -9,15 +9,17 @@ FROM customer
 GROUP BY gender;
 
 --Query 3
-select store.storename, "Transaction".qty as "Total Quantity"
+select store.storename, sum("Transaction".qty) as "Total Quantity"
 from store inner join "Transaction"
 on store.storeid = "Transaction".storeid
+group by store.storename 
 order by "Total Quantity" desc
 limit 1;
 
 --Query 4
-select product."Product Name", "Transaction".totalamount as "Total Amount"
+select product."Product Name", sum("Transaction".totalamount) as "Total Amount"
 from product inner join "Transaction"
 on product.productid = "Transaction".productid 
+group by product."Product Name" 
 order by "Total Amount" desc
 limit 1; 
